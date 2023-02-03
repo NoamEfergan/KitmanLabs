9032e01a95cc99a52ea4bff0dd91a76dd30b3e0e
Noam Efergan <efergann@gmail.com>
2023-02-03 09:00:11 +0000

    Initial Commit

---
+++ KitmanLabs/KitmanLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitmanLabsApp.swift
+//  KitmanLabs
+//
+//  Created by Noam Efergan on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct KitmanLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
